d22541b97ef455d9ffa4389a39db8d40999aa103
My Name <knuckostya@gmail.com>
2023-05-09 19:31:24 +0300

    versel

--- index.js
+++ index.js
@@ -9,34 +9,42 @@ app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'knuckostya@gmail.com',
-        pass: 'micutgnuyrttjunn',
-    },
-});
+
 
 app.get('/sendMessage', async (req, res) => {
     res.send('<div>Hello</div>')
 })
 
 app.post('/sendMessage', async (req, res) => {
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        secure: true,
+        auth: {
+            user: 'knuckostya@gmail.com',
+            pass: 'micutgnuyrttjunn',
+
+        },
+    });
+
     let {email, name, text} = req.body
 
     let info = await transporter.sendMail({
-
-        to: "knuckostya@gmail.com",
+        // from: email,
+        // to: "knuckostya@gmail.com",
+        // subject: "HR from portfolio",
+        from: 'Name',
+        to: email,
         subject: "HR from portfolio",
+
 
         html: `<b>Сообщение от HR, ${name} с PORTFOLIO</b>
                <div>${email}</div>
                <div>${text}</div>`,
     });
+
     res.set('Content-Type', 'text/html');
     res.send('<div>Hello</div>')
-
-    console.log(email, text)
 
     res.send(req.body)
 })
